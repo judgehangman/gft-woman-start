d07b77141068be86dd24c2614774706f3d89d2b2
judgehangman <judgehangman@gmail.com>
2022-05-12 22:01:33 -0300

    Cria programa de exercicio de datatypes e variaveis

---
+++ modulo-03-java-fundamental/m3-jf-exercicio-variaveis-dados/src/br/com/dio/judgehangman/TiposDados.java
@@ -0,0 +1,59 @@
+package br.com.dio.judgehangman;
+
+public class TiposDados {
+
+    public static void main(String[] args) {
+
+        // Criar duas variáveis para cada tipo apresentado
+
+        byte b1 = 10;
+        byte b2 = -3;
+
+        short s1 = 20000;
+        short s2 = -3999;
+
+        int i1 = -100485;
+        int i2 = 6;
+
+        long l1 = 1000000000000000009L;
+        long l2 = -1000000000000000009L;
+
+        float f1 = 59965.34f;
+        float f2 = 4.6f;
+
+        double d1 = 409309546.334564d;
+        double d2 = -390409.2222222;
+
+        String st1 = "String 1";
+        String st2 = "String 2";
+
+        char ch1 = 'C';
+        char ch2 ='\u0043';
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println("the value assigned to st1 is " + st1);
+        System.out.println("the value assigned to st2 is " + st2);
+        System.out.println("the value assigned to ch1 is " + ch1);
+        System.out.println("the value assigned to ch2 is " + ch2);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+
+
+    }
+
+}
